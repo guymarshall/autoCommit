2e77d6ebde46e7a521b30f3c39881d6b1a0f4f9d
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-12-09 12:30:46 +0000

    Remove commented code

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ count = int(input("How many times do you want to commit? \n"))
 auto_push = input("Auto git push when commited? (y/n) \n")
 
 for i in range(count):
-	# os.system('git commit --allow-empty -m "New Commit at: $(date)"')
 	os.system(f'git commit --allow-empty -m "Commit {i} of {count}"')	
 
 print("Commited " + str(count) + " times")
