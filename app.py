2b07be59f0f808613a4c6f85951aa52cb11d6650
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-12-09 12:44:32 +0000

    Change single-quotes to double-quotes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 import os
 
 def commit(i: int, count: int):
-	os.system(f'git commit --allow-empty -m "Commit {i} of {count}"')
+	os.system(f"git commit --allow-empty -m \"Commit {i} of {count}\"")
 
 def main():
 	count = int(input("How many times do you want to commit? "))
@@ -15,7 +15,7 @@ def main():
 	print(f"Committed {count} times")
 
 	if auto_push == "y":
-		os.system('git push')
+		os.system("git push")
 
 if __name__ == "__main__":
 	main()
