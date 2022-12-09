32447c968a285f1b8559805ffe525a6f4bf257a8
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-12-09 12:36:18 +0000

    Remove unnecessary whitespace

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ count = int(input("How many times do you want to commit? "))
 auto_push = input("Auto git push when commited? (y/n) ")
 
 for i in range(count):
-	os.system(f'git commit --allow-empty -m "Commit {i} of {count}"')	
+	os.system(f'git commit --allow-empty -m "Commit {i} of {count}"')
 
 print(f"Committed {count} times")
 
