be92783f45471ce0a9bdc5c5c00e72c963d4e626
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-12-09 12:42:07 +0000

    Rearrange code to use commit()

--- app.py
+++ app.py
@@ -2,20 +2,20 @@
 
 import os
 
+def commit(i: int, count: int):
+	os.system(f'git commit --allow-empty -m "Commit {i} of {count}"')
+
 def main():
 	count = int(input("How many times do you want to commit? "))
 	auto_push = input("Auto git push when commited? (y/n) ")
 
 	for i in range(count):
-		os.system(f'git commit --allow-empty -m "Commit {i} of {count}"')
+		commit(i, count)
 
 	print(f"Committed {count} times")
 
 	if auto_push == "y":
 		os.system('git push')
-
-def commit():
-	os.system(f'git commit --allow-empty -m "Commit {i} of {count}"')
 
 if __name__ == "__main__":
 	main()
