a751971ebb761c53e3b12296864263b8c6844af6
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-12-09 12:40:33 +0000

    Reorganise to use if name main main

--- app.py
+++ app.py
@@ -2,16 +2,20 @@
 
 import os
 
-count = int(input("How many times do you want to commit? "))
-auto_push = input("Auto git push when commited? (y/n) ")
+def main():
+	count = int(input("How many times do you want to commit? "))
+	auto_push = input("Auto git push when commited? (y/n) ")
 
-for i in range(count):
-	os.system(f'git commit --allow-empty -m "Commit {i} of {count}"')
+	for i in range(count):
+		os.system(f'git commit --allow-empty -m "Commit {i} of {count}"')
 
-print(f"Committed {count} times")
+	print(f"Committed {count} times")
 
-if auto_push == "y":
-	os.system('git push')
+	if auto_push == "y":
+		os.system('git push')
 
 def commit():
 	os.system(f'git commit --allow-empty -m "Commit {i} of {count}"')
+
+if __name__ == "__main__":
+	main()
